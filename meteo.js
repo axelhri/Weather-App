03348e28234d22e141473bbd5f7d5bf1183bf8d1
axelhri <axelhouairi@gmail.com>
2025-02-03 23:48:07 +0100

    comments added

--- meteo.js
+++ meteo.js
@@ -1,77 +1,78 @@
 const loadConfig = async () => {
   try {
-    const configResponse = await fetch("./conf.json");
-    if (!configResponse.ok) {
-      throw new Error(
-        `Erreur lors du chargement du fichier de configuration : ${configResponse.status}`
-      );
+    // fetch conf.json file
+    const response = await fetch("./conf.json");
+    if (!response.ok) {
+      throw new Error(`Erreur ${response.status}`);
     }
-    const config = await configResponse.json();
+    const config = await response.json();
 
-    if (config.city) {
-      weatherData(config.city);
-
-      setInterval(() => {
-        weatherData(config.city);
-      }, 3600000);
-    } else {
-      console.error(
-        "La ville n'est pas définie dans le fichier de configuration."
-      );
-    }
+    // call weatherData and pass the city, refresh every hours
+    config.city
+      ? (weatherData(config.city),
+        setInterval(() => {
+          weatherData(config.city);
+        }, 3600000))
+      : console.error(
+          "La ville n'est pas définie dans le fichier de configuration."
+        );
   } catch (error) {
-    console.error("Erreur lors de la lecture de la configuration :", error);
+    console.error("Erreur lors de la récupération", error);
   }
 };
 
 const weatherData = async (city) => {
-  const url = `https://api.weatherapi.com/v1/current.json?key=a740ef16321749488a095816251601&q=${city}&aqi=no`;
+  const url = `http://api.weatherapi.com/v1/current.json?key=a740ef16321749488a095816251601&q=${city}&aqi=no`;
 
   try {
+    // fetch data from api
     const response = await fetch(url);
     if (!response.ok) {
-      throw new Error(`Erreur : ${response.status}`);
+      throw new Error(`Erreur ${response.status}`);
     }
     const data = await response.json();
     console.log(data);
 
+    // get element inside HTML
     const section = document.getElementById("card");
 
     const weatherCard = `
       <h1>Weather App</h1>
 
       <div class="weather-information">
-      <img src=${data.current.condition.icon} alt="${data.current.condition.text}">
-      <p class="temperature">${data.current.temp_c} °C</p>
-      <p class="country">${data.location.country},</p>
-      <p class="city">${data.location.name}</p>
+        <img src=${data.current.condition.icon} alt="${data.current.condition.text}">
+        <p class="temperature">${data.current.temp_c} °C</p>
+        <p class="country">${data.location.country},</p>
+        <p class="city">${data.location.name}</p>
       </div>
 
       
-    <div class="box-container">
-      <div class="box">
-      <i class="fa-solid fa-temperature-three-quarters"></i>
-      <div>
-      <p class="box-number">${data.current.feelslike_c} °C</p>
-      <p class="box-text">Feels like</p>
-      </div>
-      </div>
-
-      <div class="box">
-      <i class="fa-solid fa-droplet"></i>
-      <div>
-      <p class="box-number">${data.current.humidity} %</p>
-      <p class="box-text">Humidity</p>
-      </div>
+      <div class="box-container">
+        <div class="box">
+        <i class="fa-solid fa-temperature-three-quarters"></i>
+        <div>
+          <p class="box-number">${data.current.feelslike_c} °C</p>
+          <p class="box-text">Feels like</p>
+        </div>
       </div>
 
-    </div>
+      <div class="box">
+        <i class="fa-solid fa-droplet"></i>
+        <div>
+          <p class="box-number">${data.current.humidity} %</p>
+          <p class="box-text">Humidity</p>
+        </div>
+      </div>
+
+      </div>
   `;
 
+    // insert weatherCard to section (.card id in HTML file)
     section.innerHTML = weatherCard;
   } catch (error) {
-    console.error("Erreur lors de la récupération de la météo :", error);
+    console.error("Erreur lors de la récupération", error);
   }
 };
 
+// call loadConfig function wich also call weatherData
 loadConfig();
